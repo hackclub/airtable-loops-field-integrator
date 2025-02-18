b2f41a0e89a3fff29cbde3dc4073c36a68c8bb8b
Zach Latta <zach@zachlatta.com>
2025-02-18 17:18:59 +0000

    Debounce 15 seconds

--- app/controllers/airtable/webhooks_controller.rb
+++ app/controllers/airtable/webhooks_controller.rb
@@ -3,7 +3,7 @@ module Airtable
     skip_before_action :verify_authenticity_token
 
     def receive
-      WebhookNotificationHandler.set(wait: 1.minute).perform_later(params[:base][:id], params[:webhook][:id], params[:timestamp])
+      WebhookNotificationHandler.set(wait: 15.seconds).perform_later(params[:base][:id], params[:webhook][:id], params[:timestamp])
     end
   end
 end 
