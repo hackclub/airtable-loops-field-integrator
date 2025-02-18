766938c4846746a6d2797ce5b82c41fbdec7b267
Zach Latta <zach@zachlatta.com>
2025-02-18 04:06:19 +0000

    Simplify concurrency

--- app/jobs/webhook_polling_setup_job.rb
+++ app/jobs/webhook_polling_setup_job.rb
@@ -2,8 +2,7 @@ class WebhookPollingSetupJob < ApplicationJob
   include GoodJob::ActiveJobExtensions::Concurrency
 
   good_job_control_concurrency_with(
-    total_limit: 1,
-    perform_limit: 1
+    total_limit: 1
   )
 
   def perform(base_ids: nil)
