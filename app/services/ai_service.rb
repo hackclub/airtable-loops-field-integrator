5ecbb826c680834a82a8dc961c820d8a274941f1
Zach Latta <zach@zachlatta.com>
2025-02-20 22:44:41 +0000

    Switch to gpt-4o

--- app/services/ai_service.rb
+++ app/services/ai_service.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class AiService
   API_KEY = Rails.application.credentials.openai.api_key
-  MODEL = "o3-mini"
+  MODEL = "gpt-4o"
 
   class << self
     def prompt_structured(prompt, output:)
