8067eb196d50571303571f88f98327e7a973a4f9
Zach Latta <zach@zachlatta.com>
2025-11-02 17:15:45 +0000

    Delete all files

--- app/controllers/airtable/webhooks_controller.rb
+++
@@ -1,9 +0,0 @@
-module Airtable
-  class WebhooksController < ApplicationController
-    skip_before_action :verify_authenticity_token
-
-    def receive
-      WebhookNotificationHandler.set(wait: 15.seconds).perform_later(params[:base][:id], params[:webhook][:id], params[:timestamp])
-    end
-  end
-end 
