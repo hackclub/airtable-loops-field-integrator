9b063333c069f261858508763893f9c1fe83543c
Zach Latta <zach@zachlatta.com>
2025-11-06 09:43:28 -0500

    Try switching to gpt-5-mini

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -3,5 +3,5 @@ require "ruby_llm/schema"
 
 RubyLLM.configure do |config|
   config.openai_api_key = ENV['OPENAI_API_KEY']
-  config.default_model = ENV.fetch('LLM_MODEL', 'gpt-5')
+  config.default_model = ENV.fetch('LLM_MODEL', 'gpt-5-mini')
 end
